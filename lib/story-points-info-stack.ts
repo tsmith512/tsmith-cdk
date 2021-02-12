c5788e0326ba75ac8c4fc668ec9c375b00191f95
Taylor Smith <tsmith@tsmithcreative.com>
2021-02-12 12:38:05 -0600

    Add www redirect for storypoints.info too

--- lib/story-points-info-stack.ts
+++ lib/story-points-info-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as amplify from "@aws-cdk/aws-amplify";
+import { HostedZone } from "@aws-cdk/aws-route53";
+import { HttpsRedirect } from "@aws-cdk/aws-route53-patterns";
 
 export class StoryPointsInfoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -19,5 +21,14 @@ export class StoryPointsInfoStack extends cdk.Stack {
 
     const domain = amplifyApp.addDomain('storypoints.info');
     domain.mapRoot(trunk);
+
+    const wwwRedirect = new HttpsRedirect(this, "story-points-domain-direct", {
+      targetDomain: "www.storypoints.info",
+      zone: HostedZone.fromHostedZoneAttributes(this, "storypointsinfo-zone", {
+        hostedZoneId: "Z9JQAQNOXZ7VA",
+        zoneName: "storypoints.info",
+      }),
+      recordNames: ["www.storypoints.info"],
+    });
   }
 }
