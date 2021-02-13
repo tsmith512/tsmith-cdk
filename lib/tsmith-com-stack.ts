4c844dadafa7813043dc970d04dd31c020bef3a5
Taylor Smith <tsmith@tsmithcreative.com>
2021-02-12 23:59:32 -0600

    Add in that legacy redirect map of old and set up domain redirects for www/tsmithcreative

--- lib/tsmith-com-stack.ts
+++ lib/tsmith-com-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as amplify from "@aws-cdk/aws-amplify";
-import { BasicAuth } from '@aws-cdk/aws-amplify';
+import { BasicAuth, RedirectStatus } from '@aws-cdk/aws-amplify';
+import { HostedZone } from "@aws-cdk/aws-route53";
+import { HttpsRedirect } from "@aws-cdk/aws-route53-patterns";
+
 
 export class TSmithComStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -33,5 +36,48 @@ export class TSmithComStack extends cdk.Stack {
     const domain = amplifyApp.addDomain('tsmith.com');
     domain.mapRoot(trunk);
     domain.mapSubDomain(staging, "staging");
+
+    // Pull in the legacy redirect map (how many more times will I migrate this...)
+    const legacyRedirects = [
+      ["/email-me", "/contact/"],
+      ["/article/custom-styles-wordpress-visual-editor", "/blog/2013/wordpress-theme-editor/"],
+      ["/file/theatrical-resume", "/assets/pdf/taylor-smith-resume-theatrical.pdf/"],
+      ["/article/gittin-started-pantheon-multidev-fk", "/blog/2013/pantheon-multidev/"],
+      ["/article/change-page-template-based-panels-layout", "/blog/2013/panels-page-tpl/"],
+      ["/article/nodejs-rpi-ikea-trophy-lighting", "/blog/2013/ikea-raspberry-pi-light/"],
+      ["/article/building-personalized-portfolio-experience-drupal-7", "/blog/2013/drupal-7-portfolio-experience/"],
+      ["/article/rescuing-myself-email-monster-javascript-fk", "/blog/2014/scripting-gmail/"],
+      ["/article/callboard", "/blog/2014/callboard-theatre-js/"],
+      ["/article/nth-child-mathematics", "/blog/2014/nth-child-math/"],
+      ["/article/cleaner-history-interactive-commit-building-git", "/blog/2016/git-interactive-adding/"],
+    ];
+
+    legacyRedirects.forEach((page) => {
+      amplifyApp.addCustomRule({source: page[0], target: page[1], status: RedirectStatus.PERMANENT_REDIRECT});
+    });
+
+    // Add custom 404 page
+    // NOTE: This path has to be exact to the resource (i.e. index.html) to
+    // be rendered, otherwise you just get a zero-content HTTP 404 error.
+    amplifyApp.addCustomRule({source: "/<*>", target: "/404/index.html", status: RedirectStatus.NOT_FOUND_REWRITE });
+
+
+    const wwwRedirect = new HttpsRedirect(this, "tsmith-com-domain-direct", {
+      targetDomain: "tsmith.com",
+      zone: HostedZone.fromHostedZoneAttributes(this, "tsmithcom-zone", {
+        hostedZoneId: "Z303DPVNWQ2JYE",
+        zoneName: "tsmith.com",
+      }),
+      recordNames: ["www.tsmith.com"],
+    });
+
+    const tsmithcreativeRedirect = new HttpsRedirect(this, "tsmithcreative-com-domain-direct", {
+      targetDomain: "tsmith.com",
+      zone: HostedZone.fromHostedZoneAttributes(this, "tsmithcreativecom-zone", {
+        hostedZoneId: "ZQ0T9AUIN7FT4",
+        zoneName: "tsmithcreative.com",
+      }),
+      recordNames: ["tsmithcreative.com", "www.tsmithcreative.com"],
+    });
   }
 }
