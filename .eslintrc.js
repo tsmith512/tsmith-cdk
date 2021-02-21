52332ed81fb177323bb8e4754d52dd51c1274bde
Taylor Smith <tsmith@tsmithcreative.com>
2021-02-21 00:17:26 -0600

    Lint the lambda email form handler

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,14 @@ module.exports = {
   "rules": {
     "no-magic-numbers": ["error", { "ignoreArrayIndexes": true }],
   },
+  "overrides": [
+    {
+      "files": ["*.js"],
+      "rules": {
+        "no-magic-numbers": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+      }
+    }
+  ]
 };
--- lambda/tsmith-com-emailhandler/index.js
+++ lambda/tsmith-com-emailhandler/index.js
@@ -1,4 +1,4 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const ses = new AWS.SES();
 
 const receiverEmail = process.env.EMAIL_DESTINATION;
@@ -7,37 +7,40 @@ const senderEmail = process.env.EMAIL_DESTINATION;
 const sendEmail = (event, callback) => {
   const content = JSON.parse(event.body);
 
-  ses.sendEmail({
-    Destination: {
-      ToAddresses: [ receiverEmail ],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: `Name: ${content.from}\n Email: ${content.replyto} \n\nMessage:\n ${content.message}`,
-          Charset: 'UTF-8'
-        }
+  ses.sendEmail(
+    {
+      Destination: {
+        ToAddresses: [receiverEmail],
       },
-      Subject: {
-        Data: `Website Referral Form: ${content.from}`,
-        Charset: 'UTF-8'
-      }
+      Message: {
+        Body: {
+          Text: {
+            Data: `Name: ${content.from}\n Email: ${content.replyto} \n\nMessage:\n ${content.message}`,
+            Charset: "UTF-8",
+          },
+        },
+        Subject: {
+          Data: `Website Referral Form: ${content.from}`,
+          Charset: "UTF-8",
+        },
+      },
+      Source: senderEmail,
+      ReplyToAddresses: [event.replyto],
     },
-    Source: senderEmail,
-    ReplyToAddresses: [ event.replyto ],
-  }, callback);
-}
+    callback
+  );
+};
 
 exports.handler = (event, context, callback) => {
-  console.log('Received event:', JSON.stringify(event));
+  console.log("Received event:", JSON.stringify(event));
   sendEmail(event, (err, data) => {
     callback(err, {
-      "isBase64Encoded": false,
-      "statusCode": (err) ? 500 : 200,
-      "headers": {
-        "Access-Control-Allow-Origin": "*"
+      sBase64Encoded: false,
+      statusCode: err ? 500 : 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
       },
-      "body": JSON.stringify({data, err}),
+      body: JSON.stringify({ data, err }),
     });
   });
-}
+};
